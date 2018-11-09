f7b20d725f9688a3a216ee668e7119f5a17580a6
r0ute <route-a@ya.ru>
2018-11-09 16:06:32 +0300

    add service resource

--- eureka-client/src/main/java/com/example/eurekaclient/resource/ServiceResource.java
+++ eureka-client/src/main/java/com/example/eurekaclient/resource/ServiceResource.java
@@ -1,24 +1,22 @@
 package com.example.eurekaclient.resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/service-instances")
-public class ServiceInstanceResource {
+@RequestMapping("/services")
+public class ServiceResource {
 
     @Autowired
     private DiscoveryClient discoveryClient;
 
-    @GetMapping("/{serviceId}")
-    public List<ServiceInstance> getServiceInstances(@PathVariable String serviceId) {
-        return discoveryClient.getInstances(serviceId);
+    @GetMapping
+    public List<String> getServices() {
+        return discoveryClient.getServices();
     }
 }
