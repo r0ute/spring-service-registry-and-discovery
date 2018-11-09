65d826358fb309963db27d78dd489ae50f092317
r0ute <route-a@ya.ru>
2018-11-09 15:11:47 +0300

    logging filter

--- zuul-gateway/src/main/java/com/example/zuulgateway/ZuulGatewayApplication.java
+++ zuul-gateway/src/main/java/com/example/zuulgateway/ZuulGatewayApplication.java
@@ -1,17 +1,24 @@
 package com.example.zuulgateway;
 
+import com.example.zuulgateway.filter.pre.LoggingFilter;
+import com.netflix.zuul.ZuulFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ZuulGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ZuulGatewayApplication.class, args);
-	}
+    @Bean
+    public ZuulFilter loggingFilter() {
+        return new LoggingFilter();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulGatewayApplication.class, args);
+    }
 }
