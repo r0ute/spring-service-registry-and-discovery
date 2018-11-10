cb3cd8c45c230cc6db007dff33af975749207ff9
r0ute <route-a@ya.ru>
2018-11-10 09:52:41 +0300

    fix service resource

--- support/eureka-client/src/main/java/com/example/ms/serviceregistry/client/resource/ServiceResource.java
+++ support/eureka-client/src/main/java/com/example/ms/serviceregistry/client/resource/ServiceResource.java
@@ -1,8 +1,10 @@
 package com.example.ms.serviceregistry.client.resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +20,10 @@ public class ServiceResource {
     @GetMapping
     public List<String> getServices() {
         return discoveryClient.getServices();
+    }
+
+    @GetMapping("/{id}/instances")
+    public List<ServiceInstance> getServiceInstances(@PathVariable String id) {
+        return discoveryClient.getInstances(id);
     }
 }
